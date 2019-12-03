b1be191b38ca8fb685672cd2898b8dc9eba801db
sdfsfd <sdfd@sdfsd.ff>
2019-12-03 20:16:43 +0300

    arw

--- heic_sips_tif.sh
+++ heic_sips_tif.sh
@@ -29,7 +29,7 @@ then
   path_wildcard="**/*/"
 fi
 
-exts=("jpg" "jpeg" "bmp" "png" "dng" "tif" "arw")
+exts=("tif")
 
 echoo "\tCONVERTING FILES WITH EXTENSIONS: $exts";
 if [[ $subfolders -eq 1 ]];
