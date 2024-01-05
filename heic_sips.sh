07818ebf42264fe1abc38a955eeb37bbb061859c
vneelank <vneelank@cisco.com>
2024-01-05 17:07:14 +0530

    Made formatting changes to give a better execution output on the terminal screen

--- heic_sips.sh
+++ heic_sips.sh
@@ -1,16 +1,22 @@
 #!/bin/zsh
 #set -x
 #setopt interactivecomments
+
+SECONDS=0
+printf "Start time: $(date) \n"
+echo ""
+
 setopt +o nomatch
 unsetopt CASE_GLOB #case insensitive *.
 
-echoo () {
-  echo $'\e[0;103m' $1 $'\e[0m'
+echoo() {
+  # echo $'\e[0;103m' $1 $'\e[0m'
+  echo $1
 }
 
-start=`date +%s`;
+start=$(date +%s)
 
-cd $1;
+cd $1
 
 remove=0
 subfolders=0
@@ -18,41 +24,37 @@ path_wildcard=""
 output_folder="_heic_conversion_tmp"
 files_not_converted=()
 
-if [[ "$2" = "remove" || "$3" = "remove" ]];
-then
+if [[ "$2" = "remove" || "$3" = "remove" ]]; then
   remove=1
 fi
 
-if [[ "$2" = "subfolders" || "$3" = "subfolders" ]];
-then
+if [[ "$2" = "subfolders" || "$3" = "subfolders" ]]; then
   subfolders=1
   path_wildcard="**/*/"
 fi
 
-exts=("jpg" "jpeg" "bmp" "png" "dng" "tif" "arw")
+exts=("jpg" "jpeg" "bmp" "png" "dng" "cr2" "tif" "arw")
 
-echoo "\tCONVERTING FILES WITH EXTENSIONS: $exts";
-if [[ $subfolders -eq 1 ]];
-then
-  echoo "\tSUBFOLDERS INCLUDED";
+echoo "CONVERTING FILES WITH EXTENSIONS: $exts"
+if [[ $subfolders -eq 1 ]]; then
+  echoo "SUBFOLDERS INCLUDED"
 fi
-if [[ $remove -eq 1 ]];
-then
-  echoo "\tORIGINAL FILES WILL BE REMOVED AFTER CONVERSION";
+if [[ $remove -eq 1 ]]; then
+  echoo "ORIGINAL FILES WILL BE REMOVED AFTER CONVERSION"
 fi
 
-glob_total=0;
-glob_success=0; 
-glob_deleted=0;
+glob_total=0
+glob_success=0
+glob_deleted=0
 
-mkdir $output_folder;
+mkdir $output_folder
 
-for ext in $exts; do 
-  echoo "\t";
-  echoo "\tPROCESSING $ext";
+for ext in $exts; do
+  echoo ""
+  echoo "PROCESSING $ext"
 
-  total_ext=0;
-  success_ext=0; 
+  total_ext=0
+  success_ext=0
 
   #echo $~path_wildcard;
 
@@ -60,63 +62,63 @@ for ext in $exts; do
 
     success_dir=0
 
-    if [[ "$dir" = "$output_folder/" ]]; #ignore output folder
-    then
+    if [[ "$dir" = "$output_folder/" ]]; then #ignore output folder
       continue
     fi
 
-    src_files=($dir*.$ext);
+    src_files=($dir*.$ext)
 
-    if [[ "${src_files[@]:0:1}" = "$dir*.$ext" ]] #no files in directory
-    then
+    if [[ "${src_files[@]:0:1}" = "$dir*.$ext" ]]; then #no files in directory
       continue
     fi
 
-    total_dir=${#src_files[@]};
-    ((total_ext=total_ext+total_dir))
+    total_dir=${#src_files[@]}
+    ((total_ext = total_ext + total_dir))
 
-    echoo "\t"
+    echoo ""
     echoo "\tCURRENT DIR: $dir, TOTAL FILES: $total_dir, RUNNING SIPS..."
-    
+
     echo ""
-    sips -s format heic $src_files --out $output_folder
+    echo "\t $src_files"
+    sips -s format heic $src_files --out $output_folder &>conversion_run.log
     echo ""
 
     dst_files=($output_folder/*.heic)
-    #echoo \tDst files: $dst_files
-    #echoo "${dst_files[@]:0:1}" 
+    #echoo Dst files: $dst_files
+    #echoo "${dst_files[@]:0:1}"
     #echoo "$output_folder/*.heic"
 
-    if [[  "${dst_files[@]:0:1}" != "$output_folder/*.heic" ]] #there're files in directory
-    then
-      success_dir=${#dst_files[@]};
-      ((success_ext=success_ext+success_dir));
-  #
-  # MOVE CONVERTED FILES
-  #
-    echoo "\tCOPYING CONVERTED FILES.."
-    for dst_file in $dst_files; do
-      dst=$(basename -- "$dst_file")
-      #echoo $dst 
-      trg_file=$dst
-      until
-        [[ ! -f $dir/$trg_file ]]
-      do
-        trg_file="$(echo "$dst" | sed -e 's/\.[^.]*$//')-dup.heic"
-      done
+    if [[ "${dst_files[@]:0:1}" != "$output_folder/*.heic" ]]; then #there're files in directory
+      success_dir=${#dst_files[@]}
+      ((success_ext = success_ext + success_dir))
+      #
+      # MOVE CONVERTED FILES
+      #
+      echoo "\tCOPYING CONVERTED FILES.."
+      for dst_file in $dst_files; do
+        dst=$(basename -- "$dst_file")
+        #echoo $dst
+        trg_file=$dst
+        until
+          [[ ! -f $dir/$trg_file ]]
+        do
+          trg_file="$(echo "$dst" | sed -e 's/\.[^.]*$//')-dup.heic"
+        done
 
-      #echoo $trg_file
-      #echoo $output_folder
-      #echoo $dir
-      mv $output_folder/$dst $dir/$trg_file 
-    done
-  #
-  # DELETE ORIGINALS
-  #
+        #echoo $trg_file
+        #echoo $output_folder
+        #echoo $dir
+        echo "\t\t$trg_file"
+        mv $output_folder/$dst $dir/$trg_file
+      done
+      #
+      # DELETE ORIGINALS
+      #
       if [[ $remove -eq 1 ]]; then
+        echo ""
         echoo "\tDELETING ORIGINALS.."
         for dst_file in $dst_files; do
-          orig_file=$(basename -- "$(echo "$dst_file" | sed -e 's/\.[^.]*$//').$ext");
+          orig_file=$(basename -- "$(echo "$dst_file" | sed -e 's/\.[^.]*$//').$ext")
           #echoo $dst_file
           #echoo $orig_file
 
@@ -124,37 +126,42 @@ for ext in $exts; do
 
           if [[ -f $dir/$orig_file ]]; then
             file_exists=1
-          fi;
+          fi
 
-          #echoo "\tOriginal file $orig_file ---- exists $file_exists" 
+          #echoo "Original file $orig_file ---- exists $file_exists"
 
           if [[ $file_exists -eq 1 ]]; then
-            rm $dir/$orig_file;
+            echo "\t\t $orig_file"
+            rm -f $dir/$orig_file
             if [[ $? -eq 0 ]]; then
               ((glob_deleted++))
             fi
           fi
-        done;
+        done
       fi
     fi
 
-    echoo "\tTOTAL FILES SUCCESSFULY CONVERTED: $success_dir";
+    echo ""
+    echoo "\t TOTAL FILES SUCCESSFULY CONVERTED: $success_dir"
 
     #mv $dst_files $dir;
     #dst="$(echo "$src" | sed -e 's/\.[^.]*$//').heic"; # remove original extension and replace it with heic
-      
-  done;
 
-  ((glob_total=total_ext+glob_total));
-  ((glob_success=success_ext+glob_success));
-  echoo "\tFILES PROCESSED SO FAR: $glob_total,  SUCCESSFULY: $glob_success, ORIGINALS DELETED: $glob_deleted";
-  echoo "\t";
-done;
+  done
 
-rm -rf $output_folder;
+  ((glob_total = total_ext + glob_total))
+  ((glob_success = success_ext + glob_success))
+  echoo "FILES PROCESSED SO FAR: $glob_total,  SUCCESSFULY: $glob_success, ORIGINALS DELETED: $glob_deleted"
+  echoo ""
+done
 
-#echoo "\tFILES NOT CONVERTED: ${#files_not_converted[@]}"
+rm -rf $output_folder
+
+#echoo "FILES NOT CONVERTED: ${#files_not_converted[@]}"
 #echoo "$files_not_converted"
 
-end=`date +%s`;
-echoo "\tTIME: `expr $end - $start` seconds.";
+# end=$(date +%s)
+# echoo "TIME: $(expr $end - $start) seconds."
+# sleep 65
+duration=$SECONDS
+printf "End time: $(date), took $(($duration / 60)) minutes or $duration seconds for processing \n"
